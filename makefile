787424aa40b9e6b15e6f512c3c31960e553198eb
Iwan Vosloo <iwan@reahl.org>
2023-09-15 08:49:25 +0200

    Switched to postgres; added necessary reahl dependencies

--- makefile
+++ makefile
@@ -3,7 +3,7 @@ OUT=$(PWD)/dist
 TOMLS:=$(shell find . -maxdepth 2 -name '*toml')
 WHLS:=$(TOMLS:%.toml=%.whl)
 
-wheels: $(WHLS)
+all: $(WHLS)
 
 clean:
 	python -m pip uninstall -y $(TOMLS:./%/pyproject.toml=%)
@@ -11,12 +11,8 @@ clean:
 	rm -rf */*egg-info
 
 
-#$(WHLS): %.whl: %.toml
-#	cd $(shell dirname $<) && python -m build --outdir $(OUT) && cd .. && touch $@
-
 $(WHLS): %.whl: %.toml
 	cd $(shell dirname $<) && python -m pip install --no-deps -e . && cd .. && touch $@
-
 
 $(OUT):
 	mkdir $(OUT)
--- myproject/etc/reahl.config.py
+++ myproject/etc/reahl.config.py
@@ -3,9 +3,10 @@ from reahl.sqlalchemysupport import SqlAlchemyControl
 
 reahlsystem.root_egg = 'myproject'
 #reahlsystem.connection_uri = 'postgresql:///reahl'
-reahlsystem.connection_uri = 'sqlite:////tmp/test.db'
+#reahlsystem.connection_uri = 'sqlite:////tmp/test.db'
 #reahlsystem.connection_uri = 'sqlite:///:memory:'
 #reahlsystem.connection_uri = os.environ.get('REAHL_TEST_CONNECTION_URI', 'postgresql:///reahl')
+reahlsystem.connection_uri = 'postgresql://reahl:reahl@postgres/reahl'
 
 reahlsystem.orm_control = SqlAlchemyControl(echo=False)
 
